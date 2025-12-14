b5d98a2d320bd7ee93885d077a8e9e5999c886e4
Dima <dima.semenovych@gmail.com>
2025-12-14 20:38:51 +0000

    hw7

--- js/task-4.js
+++ js/task-4.js
@@ -4,13 +4,21 @@ loginForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault(); 
-  const form = event.target; 
-  const email = form.elements.email.value.trim(); 
-  const password = form.elements.password.value.trim(); 
-  
+
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
   if (email === "" || password === "") {
-    return console.log("Please fill in all the fields!");
+    alert("All form fields must be filled in");
+    return;
   }
-  console.log(`Email: ${email}, Password: ${password}`);
-  form.reset(); 
+
+  const userData = {
+    email: email,
+    password: password,
+  };
+
+  console.log(userData);
+  form.reset();
 }
